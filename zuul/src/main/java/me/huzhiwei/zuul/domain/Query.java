e7387b9db1b503929a65fbbe091dd45c20620a7f
HuZhiwei <huzw@silvrr.com>
2019-08-12 16:20:26 +0800

    add MonitorController

---
+++ zuul/src/main/java/me/huzhiwei/zuul/domain/Query.java
@@ -0,0 +1,14 @@
+package me.huzhiwei.zuul.domain;
+
+import lombok.Data;
+
+/**
+ * description:
+ * author: koshitakashi
+ * date: 2019-08-12 15:46
+ */
+@Data
+public class Query {
+    private int page;
+    private int size;
+}
---
+++ zuul/src/main/java/me/huzhiwei/zuul/domain/RequestQuery.java
@@ -0,0 +1,16 @@
+package me.huzhiwei.zuul.domain;
+
+import lombok.Data;
+
+/**
+ * description:
+ * author: koshitakashi
+ * date: 2019-08-12 15:46
+ */
+@Data
+public class RequestQuery extends Query {
+    private String groupId;
+    private String routeId;
+    private Long beginTime;
+    private Long endTime;
+}
