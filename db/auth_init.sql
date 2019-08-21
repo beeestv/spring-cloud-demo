279df0fe7dfd2c1bc593c25b61522dea3260fa85
HuZhiwei <huzw@silvrr.com>
2019-08-21 15:40:24 +0800

    更新数据库初始化脚本

--- db/auth_init.sql
+++ db/auth_init.sql
@@ -5,16 +5,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 --  Table structure for `oauth_access_token`
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_access_token`;
-CREATE TABLE `oauth_access_token` (
-  `token_id` varchar(256) DEFAULT NULL,
-  `token` blob,
-  `authentication_id` varchar(128) NOT NULL,
-  `user_name` varchar(256) DEFAULT NULL,
-  `client_id` varchar(256) DEFAULT NULL,
-  `authentication` blob,
-  `refresh_token` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`authentication_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `oauth_access_token`
+(
+    `token_id`          varchar(256) DEFAULT NULL,
+    `token`             blob,
+    `authentication_id` varchar(128) NOT NULL,
+    `user_name`         varchar(256) DEFAULT NULL,
+    `client_id`         varchar(256) DEFAULT NULL,
+    `authentication`    blob,
+    `refresh_token`     varchar(256) DEFAULT NULL,
+    PRIMARY KEY (`authentication_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Records of `oauth_access_token`
@@ -27,65 +29,76 @@ CREATE TABLE `oauth_access_token` (
 --  Table structure for `oauth_approvals`
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_approvals`;
-CREATE TABLE `oauth_approvals` (
-  `userId` varchar(256) DEFAULT NULL,
-  `clientId` varchar(256) DEFAULT NULL,
-  `scope` varchar(256) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
-  `expiresAt` datetime DEFAULT NULL,
-  `lastModifiedAt` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `oauth_approvals`
+(
+    `userId`         varchar(256) DEFAULT NULL,
+    `clientId`       varchar(256) DEFAULT NULL,
+    `scope`          varchar(256) DEFAULT NULL,
+    `status`         varchar(10)  DEFAULT NULL,
+    `expiresAt`      datetime     DEFAULT NULL,
+    `lastModifiedAt` datetime     DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Table structure for `oauth_client_details`
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_client_details`;
-CREATE TABLE `oauth_client_details` (
-  `client_id` varchar(256) NOT NULL,
-  `resource_ids` varchar(256) DEFAULT NULL,
-  `client_secret` varchar(256) DEFAULT NULL,
-  `scope` varchar(256) DEFAULT NULL,
-  `authorized_grant_types` varchar(256) DEFAULT NULL,
-  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
-  `authorities` varchar(256) DEFAULT NULL,
-  `access_token_validity` int(11) DEFAULT NULL,
-  `refresh_token_validity` int(11) DEFAULT NULL,
-  `additional_information` varchar(4096) DEFAULT NULL,
-  `autoapprove` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`client_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `oauth_client_details`
+(
+    `client_name`             varchar(256) NOT NULL,
+    `client_id`               varchar(256) NOT NULL,
+    `resource_ids`            varchar(256)  DEFAULT NULL,
+    `client_secret`           varchar(256)  DEFAULT NULL,
+    `scope`                   varchar(256)  DEFAULT NULL,
+    `authorized_grant_types`  varchar(256)  DEFAULT NULL,
+    `web_server_redirect_uri` varchar(256)  DEFAULT NULL,
+    `authorities`             varchar(256)  DEFAULT NULL,
+    `access_token_validity`   int(11)       DEFAULT NULL,
+    `refresh_token_validity`  int(11)       DEFAULT NULL,
+    `additional_information`  varchar(4096) DEFAULT NULL,
+    `autoapprove`             varchar(256)  DEFAULT NULL,
+    PRIMARY KEY (`client_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Table structure for `oauth_client_token`
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_client_token`;
-CREATE TABLE `oauth_client_token` (
-  `token_id` varchar(256) DEFAULT NULL,
-  `token` blob,
-  `authentication_id` varchar(128) NOT NULL,
-  `user_name` varchar(256) DEFAULT NULL,
-  `client_id` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`authentication_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `oauth_client_token`
+(
+    `token_id`          varchar(256) DEFAULT NULL,
+    `token`             blob,
+    `authentication_id` varchar(128) NOT NULL,
+    `user_name`         varchar(256) DEFAULT NULL,
+    `client_id`         varchar(256) DEFAULT NULL,
+    PRIMARY KEY (`authentication_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Table structure for `oauth_code`
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_code`;
-CREATE TABLE `oauth_code` (
-  `code` varchar(256) DEFAULT NULL,
-  `authentication` blob
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `oauth_code`
+(
+    `code`           varchar(256) DEFAULT NULL,
+    `authentication` blob
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Table structure for `oauth_refresh_token`
 -- ----------------------------
 DROP TABLE IF EXISTS `oauth_refresh_token`;
-CREATE TABLE `oauth_refresh_token` (
-  `token_id` varchar(256) DEFAULT NULL,
-  `token` blob,
-  `authentication` blob
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `oauth_refresh_token`
+(
+    `token_id`       varchar(256) DEFAULT NULL,
+    `token`          blob,
+    `authentication` blob
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Records of `oauth_refresh_token`
@@ -98,54 +111,67 @@ CREATE TABLE `oauth_refresh_token` (
 --  Table structure for `role`
 -- ----------------------------
 DROP TABLE IF EXISTS `role`;
-CREATE TABLE `role` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+CREATE TABLE `role`
+(
+    `id`   bigint(20)   NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 3
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Records of `role`
 -- ----------------------------
 BEGIN;
-INSERT INTO `role` VALUES ('1', 'ROLE_USER'), ('2', 'ROLE_ADMIN');
+INSERT INTO `role`
+VALUES ('1', 'ROLE_USER'),
+       ('2', 'ROLE_ADMIN');
 COMMIT;
 
 -- ----------------------------
 --  Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `password` varchar(255) DEFAULT NULL,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+CREATE TABLE `user`
+(
+    `id`       bigint(20)   NOT NULL AUTO_INCREMENT,
+    `password` varchar(255) DEFAULT NULL,
+    `username` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 7
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('1', '123456', 'huzhiwei');
+INSERT INTO `user`
+VALUES ('1', '123456', 'admin');
 COMMIT;
 
 -- ----------------------------
 --  Table structure for `user_role`
 -- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
-CREATE TABLE `user_role` (
-  `user_id` bigint(20) NOT NULL,
-  `role_id` bigint(20) NOT NULL,
-  KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
-  KEY `FK859n2jvi8ivhui0rl0esws6o` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `user_role`
+(
+    `user_id` bigint(20) NOT NULL,
+    `role_id` bigint(20) NOT NULL,
+    KEY `roleId` (`role_id`),
+    KEY `userId` (`user_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
 -- ----------------------------
 --  Records of `user_role`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user_role` VALUES ('1', '1'), ('1', '2');
+INSERT INTO `user_role`
+VALUES ('1', '1'),
+       ('1', '2');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
--- db/zuul_init.sql
+++ db/zuul_init.sql
@@ -1,17 +1,6 @@
 CREATE DATABASE IF NOT EXISTS zuul DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 use zuul;
 
-DROP TABLE IF EXISTS `route_group`;
-CREATE TABLE `route_group`
-(
-    `id`          bigint(11)  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `name`        varchar(50) NOT NULL,
-    `online`      tinyint(1)  NOT NULL,
-    `description` text        NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
-
 DROP TABLE IF EXISTS `route`;
 CREATE TABLE `route`
 (
@@ -19,12 +8,8 @@ CREATE TABLE `route`
     `name`         varchar(50)  NOT NULL,
     `path`         varchar(255) NOT NULL,
     `service_id`   varchar(255) DEFAULT NULL,
-    `url`          varchar(255) DEFAULT NULL,
     `strip_prefix` tinyint(1)   NOT NULL,
-    `group_id`     bigint(11)   NOT NULL,
-    UNIQUE KEY (`name`),
-    UNIQUE KEY (`path`),
-    KEY (`group_id`)
+    UNIQUE KEY (`path`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
